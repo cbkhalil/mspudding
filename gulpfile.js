80e88340582532a56d642ef517396faf0ad9de52
Brynn Khalil <cbkhalil@Brynns-MacBook.local>
2016-07-19 15:07:08 -0700

    removing cached files

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+const config = {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+}
+
+const gulp 			= require('gulp')
+const inline 		= require('gulp-inline')
+const pug 			= require('gulp-pug')
+const minifyCSS 	= require('gulp-minify-css')
+const uglify 		= require('gulp-uglify')
+const s3 			= require('gulp-s3-upload')(config)
+
+gulp.task("upload", function() {
+    return 	gulp.src("/Client/index.html")
+		    .pipe(s3({
+		        Bucket: process.env.AWS_S3_BUCKET_NAME,
+		        ACL:    'public-read'
+		    }, 
+		    {
+		        maxRetries: 5
+		    }))
+})
+
+
+
+gulp.task('default', ['upload'])
