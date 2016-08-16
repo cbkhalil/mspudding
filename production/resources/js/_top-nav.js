92a8533ac4e984e8ee939b28102779193241a132
Christine Brynn <christine.khalil@gmail.com>
2016-08-16 16:58:16 -0700

    fixed some css issues and added reset.scss to site.scss

--- production/resources/js/_top-nav.js
+++ production/resources/js/_top-nav.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
     var collapsed = true
     $('#navbar-dropdown-button').click(function() {
         collapsed ? $('#navbar-dropdown-menu').removeClass('hide') : $('#navbar-dropdown-menu').addClass('hide');
-        collapsed = !collapsed
+        collapsed = !collapsed;
     });
 });
