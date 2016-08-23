51524ffabcee2ff75ade1d497cfef869917742e1
Christine Brynn <christine.khalil@gmail.com>
2016-08-23 10:23:24 -0700

    set up gulp stuff, and layout file

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,19 @@ const config    = {
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 }
 
-const gulp      = require('gulp')
-const sass      = require('gulp-sass')
-const pug       = require('gulp-pug')
-const s3        = require('gulp-s3-upload')(config)
+const gulp          = require('gulp')
+const sass          = require('gulp-sass')
+const pug           = require('gulp-pug')
+const s3            = require('gulp-s3-upload')(config)
+const argv          = require('yargs').argv
+const browserSync   = require('browser-sync').create();
 
-gulp.task('render-blog', function() {
+
+
+
+
+
+gulp.task('render-blog', () => {
   return  gulp.src('./development/blog/*.pug')
     .pipe(pug({
       doctype: 'html'
@@ -16,7 +23,7 @@ gulp.task('render-blog', function() {
     .pipe(gulp.dest('./production/blog/'))
 })
 
-gulp.task('render-includes', function() {
+gulp.task('render-includes', () => {
   return  gulp.src('./development/includes/*.pug')
     .pipe(pug({
       doctype: 'html'
@@ -24,17 +31,44 @@ gulp.task('render-includes', function() {
     .pipe(gulp.dest('./production/'))
 })
 
-gulp.task('sass', function () {
+gulp.task('pug:watch', ['render-blog', 'render-includes'], () => {
+  gulp.watch(['./development/blog/*.pug', './development/includes/*.pug'], ['render-blog', 'render-includes']);
+});
+
+
+
+
+
+
+
+gulp.task('sass', () => {
   return gulp.src('./development/sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./production/resources/css/'));
 });
 
-// gulp.task('sass:watch', function () {
-//   gulp.watch('./development/sass/*.scss', ['sass']);
-// });
+gulp.task('sass:watch', ['sass'], () => {
+  gulp.watch('./development/sass/**/*.scss', ['sass']);
+});
 
-gulp.task("upload", ['render-blog', 'render-includes', 'sass'], function() {
+
+
+
+
+gulp.task('serve', () => {
+    browserSync.init({
+        server: {
+            baseDir: "./production"
+        },
+        open: false
+    });
+});
+
+
+
+
+
+gulp.task("upload", ['render-blog', 'render-includes', 'sass'], () => {
   return  gulp.src("./production/")
     .pipe(s3({
       Bucket: process.env.AWS_S3_BUCKET_NAME,
@@ -46,5 +80,4 @@ gulp.task("upload", ['render-blog', 'render-includes', 'sass'], function() {
 })
 
 
-
-gulp.task('default', ['upload'])
+gulp.task('default', argv.deploy ? ['upload'] : ['sass:watch', 'pug:watch', 'serve'])
